b8a335bb45e11e8bacdc2c75a68b9bee44006bd2
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-03 10:56:11 +0530

    done

--- playground.js
+++ playground.js
@@ -1,27 +1,14 @@
-//  Only numbers. Double-precision floating point units.
+//  named function
+function namedFoo(param) {
+  //...
+}
 
-let i = 100.1222;
-
-console.log(typeof i);
-
-//  Divide by zero is not an exception, but a special value
-console.log(i / 0);
-
-//  a variable can change its type in the course of the program
-i = 'title';
-console.log(typeof i);
-
-i = false;
-console.log(typeof i);
-
-i = {
-  name: 'Vijay',
-  city: 'Mangaluru'
-};
-
-console.log(typeof i);
-
-i = function() {
+//  anonymous function/expression
+let functionExpression = function(param) {
   //...
 };
-console.log(typeof i);
+
+//  lambda expressions
+let lambdaExpression = param => {
+  //...
+};
