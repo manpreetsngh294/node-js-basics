bd2b895d2ef312ebd9ac1940efac7867966476b4
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-03 11:41:49 +0530

    done

--- playground.js
+++ playground.js
@@ -1,16 +1,4 @@
-let car = {
-  model: 'MG Hector',
-  owner: {
-    name: 'Sunil',
-    city: 'Bengaluru'
-  },
-  drive: function() {
-    console.log('drives');
-  },
-  wheels: ['RX19', 'RY19']
-};
-
-console.log(car.model);
-console.log(car.owner.name.lastName);
-
-car.drive();
+//  IIFE: Immediately Invoked Function Expression
+(function() {
+  console.log('IIFE');
+})();
