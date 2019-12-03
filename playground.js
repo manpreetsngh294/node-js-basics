78878833d317896b63f7fcb1c684c6baee4d2f9a
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-03 11:04:50 +0530

    done

--- playground.js
+++ playground.js
@@ -1,14 +1,17 @@
-//  named function
+//  Call is legal even for: too many parameters, too few parameters
+//  Cannot enforce the types of parameters
+
+//  Usual boiler-plate in case type should be enforced
 function namedFoo(param) {
-  //...
+  if (param) {
+    if (typeof param === 'number') {
+      console.log(param);
+    } else {
+      console.log('pass a number');
+    }
+  } else {
+    console.log('no parameter passed');
+  }
 }
 
-//  anonymous function/expression
-let functionExpression = function(param) {
-  //...
-};
-
-//  lambda expressions
-let lambdaExpression = param => {
-  //...
-};
+namedFoo();
